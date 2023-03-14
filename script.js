cc07e0d2852df8ba8c9972ea6992d01cf564c4f7
jCasia <casjay70@gmail.com>
2023-03-14 23:05:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const topHeading = document.querySelector(".top-heading");
+const botHeading = document.querySelector(".bot-heading");
+const navLinks = document.querySelectorAll(".header__link");
+const logo = document.querySelector(".header__logo");
+const heroImg = document.querySelector(".hero__img");
+const arrow = document.querySelector(".arrow");
+
+// window.addEventListener("beforeunload", () => {
+//   window.scrollTo(0, 0);
+// });
+
+const revealAnimation = () => {
+  const timelineFade = gsap.timeline();
+
+  gsap.to(arrow, {
+    keyframes: [
+      { y: 25, duration: 1 },
+      { y: 0, duration: 1 },
+      { y: 25, duration: 1 },
+      { y: 0, duration: 1 },
+    ],
+    repeat: Infinity,
+  });
+
+  timelineFade
+    .from(topHeading, { autoAlpha: 0, x: -50, delay: 0.1, duration: 0.75 })
+    .from(heroImg, { autoAlpha: 0, duration: 0.75 }, "-=0.35")
+    .from(botHeading, { autoAlpha: 0, x: -50, duration: 0.75 }, "-=0.5")
+    .from(arrow, { autoAlpha: 0, duration: 0.75 }, "-=0.5")
+    .from(
+      navLinks,
+      { autoAlpha: 0, y: -50, duration: 0.4, stagger: 0.1 },
+      "-=0.25"
+    )
+    .from(logo, { autoAlpha: 0, y: -50 }, "<");
+};
+
+window.addEventListener("load", revealAnimation);
+
+//
+
+const box = document.querySelector(".box");
+const tools = document.querySelector(".toggle-wrapper");
+
+const boxReveal = () => {
+  tools.classList.toggle("box-toggle");
+
+  if (tools.classList.contains("box-toggle")) {
+    box.classList.replace("fa-box", "fa-box-open");
+  } else {
+    box.classList.replace("fa-box-open", "fa-box");
+  }
+};
+
+box.addEventListener("click", boxReveal);
